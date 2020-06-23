da39e19e3be00aac6a4e15c40caee2040d140018
rag-lab <emaildorodrigo@yahoo.com>
2020-06-23 00:50:57 -0300

    commit inicial

--- src/components/FloatingCart/index.tsx
+++ src/components/FloatingCart/index.tsx
@@ -25,14 +25,21 @@ const FloatingCart: React.FC = () => {
 
   const cartTotal = useMemo(() => {
     // TODO RETURN THE SUM OF THE PRICE FROM ALL ITEMS IN THE CART
+    const total = products.reduce((accumulator, product) => {
+      const productSubtotal = product.price * product.quantity;
+      return accumulator + productSubtotal;
+    }, 0);
 
-    return formatValue(0);
+    return formatValue(total);
   }, [products]);
 
   const totalItensInCart = useMemo(() => {
     // TODO RETURN THE SUM OF THE QUANTITY OF THE PRODUCTS IN THE CART
+    const totalItems = products.reduce((acc, product) => {
+      return acc + product.quantity;
+    }, 0);
 
-    return 0;
+    return totalItems;
   }, [products]);
 
   return (
